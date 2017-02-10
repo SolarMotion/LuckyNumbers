08ce43eadda2d972073d30260d72f46dfdb42ed9
SolarMotion <ywc92@hotmail.com>
2017-02-10 12:06:47 +0800

    .

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace LuckyNumbers
 {
@@ -10,6 +11,29 @@ namespace LuckyNumbers
     {
         static void Main(string[] args)
         {
+            var random = new Random();
+            var allIntegers = new List<int>();
+            var selectedIntegers = new List<int>();
+
+            for (int i = 1; i < 64; i++)
+                allIntegers.Add(i);
+
+            while (selectedIntegers.Count < 6)
+            {
+                var randomNumber = random.Next(0, 62);
+
+                var selectedInteger = allIntegers[randomNumber];
+
+                if (!selectedIntegers.Any(a => a.Equals(selectedInteger)))
+                    selectedIntegers.Add(selectedInteger);
+            }
+
+            selectedIntegers = selectedIntegers.OrderBy(a => a).ToList();
+            var finalResult = string.Join(", ", selectedIntegers);
+
+            WriteLine($"The lucky numbers is: {finalResult}");
+
+            ReadLine();
         }
     }
 }
